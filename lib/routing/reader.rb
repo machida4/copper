5dd84a6f4e9202b85afd2c69cb5d5f6ca64cb000
machida <machida@kmc.gr.jp>
2020-09-06 00:47:16 +0900

    deep_mergeはrefineで扱うように変更

--- lib/routing/reader.rb
+++ lib/routing/reader.rb
@@ -1,15 +1,19 @@
 require "pathname"
 
-# ネストされたハッシュを破壊的にマージする
-# TODO: あとで隔離
-class ::Hash
-  def deep_merge!(second)
-    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
-    self.merge!(second.to_h, &merger)
+module HashHelper
+  # ネストされたハッシュを破壊的にマージする
+  # TODO: あとで隔離?
+  refine ::Hash do
+    def deep_merge!(second)
+      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
+      self.merge!(second.to_h, &merger)
+    end
   end
 end
 
 module Routing
+  using HashHelper
+
   class Reader
     ALLOWED_METHODS = [:get, :post, :put, :delete]
 
